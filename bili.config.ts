41b788e6023aeb2918c0bbc780a93ed75e37fde4
Adam Dickinson <me@adamdickinson.com.au>
2021-05-28 23:05:52 +1000

    build: upgrade and switch to rollup

--- bili.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from 'bili'
-
-const config: Config = {
-  input: 'src/index.tsx'
-}
-
-export default config
---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
+import cleanup from 'rollup-plugin-cleanup'
+
+export default {
+  input: 'src/index.tsx',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+    sourcemap: true,
+  },
+  plugins: [
+    typescript({ target: 'es5' }),
+    cleanup({ comments: 'none' }),
+    terser(),
+  ],
+}
